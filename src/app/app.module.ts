f3229015e63431119215eee59aea318a2e580d49
Christian Sosa <chrissm28@outlook.com>
2022-05-28 02:37:47 -0500

    Primer Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { SharedModule } from './shared/shared.module';
+import { PaisModule } from './pais/pais.module';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [AppComponent],
+  imports: [BrowserModule, SharedModule, PaisModule],
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
