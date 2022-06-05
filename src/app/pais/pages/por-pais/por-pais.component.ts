955f3f2abf586d1bf1847caef79448a627396da8
Christian Sosa <chrissm28@outlook.com>
2022-06-04 19:26:02 -0500

    creados componentes tabla e input

--- src/app/pais/pages/por-pais/por-pais.component.ts
+++ src/app/pais/pages/por-pais/por-pais.component.ts
@@ -15,9 +15,11 @@ export class PorPaisComponent {
 
   constructor(private paisService: PaisService) {}
 
-  buscar() {
+  buscar(termino: string) {
     this.hayError = false;
-    this.paisService.buscarPais(this.termino).subscribe(
+    this.termino = termino;
+
+    this.paisService.buscarPais(termino).subscribe(
       (paises) => {
         this.paises = paises;
       },
